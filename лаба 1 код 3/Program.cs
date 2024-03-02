29ba5faae2f42e173b0d0af7b86327a225fa1c46
red1qdead <red1qdead@gmail.com>
2024-03-02 11:52:18 +0200

    Добавьте файлы проекта.

---
+++ лаба 1 код 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { -20, 15, -10, 17, 22, 35, 50, 75, 100, 3, 99, 58 };
+        var positiveTwoDigits = numbers.Where(n => n >= 10 && n <= 99).OrderBy(n => n).ToList();
+
+        Console.WriteLine(string.Join(", ", positiveTwoDigits));
+
+        Console.WriteLine("Натиснiть будь-яку клавiшу для виходу...");
+        Console.ReadLine();
+    }
+}
